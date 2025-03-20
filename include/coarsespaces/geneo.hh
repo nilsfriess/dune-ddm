af131335eb4a1ec1c1a31b123ec1ef86f7b034eb
Nils Friess <nils.friess@gmail.com>
2025-03-20 20:10:49 +0100

    [WIP] Implement GenEO on the ring (extension of solution to subdomain interior is still missing)

--- include/coarsespaces/geneo.hh
+++ include/coarsespaces/geneo.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <dune/common/parallel/interface.hh>
 #include <dune/common/parallel/variablesizecommunicator.hh>
 #include <dune/common/parametertree.hh>
@@ -8,7 +9,6 @@
 #include <dune/istl/preconditioners.hh>
 #include <dune/istl/umfpack.hh>
 
-#include <memory>
 #include <spdlog/spdlog.h>
 
 #include <vector>
@@ -25,33 +25,12 @@ std::vector<Vec> buildGenEOCoarseSpace(const RemoteIndices &ovlp_ids, const Mat 
   auto *eigensolver_event = Logger::get().registerEvent("GenEO", "solve eigenproblem");
   auto *neumann_corr = Logger::get().registerEvent("GenEO", "setup Neumann");
 
-  spdlog::info("Setting up GenEO coarse space");
-
-  // For the GenEO coarse space, we first need to assemble the Neumann matrix on the overlapping subdomain.
-  // This can be done by copying the Dirichlet matrix and applying the Neumann correction that we created
-  // earlier.
+  auto geneo_type = ptree.get("geneo_type", "full");
+  spdlog::info("Setting up GenEO coarse space in mode '{}'", geneo_type);
   Logger::get().startEvent(neumann_corr);
-  auto A = Aovlp;
 
-  const auto &ovlp_paridxs = *ovlp_ids.second;
-  for (const auto &triple : remote_ncorr_triples) {
-    // The triples use global indices, so we first have to convert them to local indices
-    // on the overlapping subdomain. Also, we might have received some indices that are
-    // outside of our overlapping subdomain, so we first have to check that.
-    if (ovlp_paridxs.exists(triple.row) && ovlp_paridxs.exists(triple.col)) {
-      auto lrow = ovlp_paridxs[triple.row].local();
-      auto lcol = ovlp_paridxs[triple.col].local();
-
-      // assert(lrow >= ovlp_paridxs.size() && lcol >= ovlp_paridxs.size() && "Should only change entries outside of the original domain");
-
-      A[lrow][lcol] -= triple.val;
-    }
-    else {
-      spdlog::trace("Global index ({}, {}) does not exist in subdomain", triple.row, triple.col);
-    }
-  }
-
-  Vec dirichlet_mask_ovlp(A.N());
+  // We begin by extending the Dirichlet mask to the overlapping subdomain
+  Vec dirichlet_mask_ovlp(Aovlp.N());
   dirichlet_mask_ovlp = 0;
   for (std::size_t i = 0; i < dirichlet_mask_novlp.N(); ++i) {
     dirichlet_mask_ovlp[i] = dirichlet_mask_novlp[i];
@@ -65,96 +44,237 @@ std::vector<Vec> buildGenEOCoarseSpace(const RemoteIndices &ovlp_ids, const Mat 
   advdh.setVec(dirichlet_mask_ovlp);
   communicator.forward(advdh);
 
-  for (std::size_t i = 0; i < A.N(); ++i) {
-    if (dirichlet_mask_ovlp[i] > 0) {
-      for (auto ci = A[i].begin(); ci != A[i].end(); ++ci) {
-        *ci = (ci.index() == i) ? 1.0 : 0.0;
+  // We begin by assigning to some dofs their distance to the overlapping subdomain boundary. This information
+  // is used in some places below.
+  // TODO: Actually, I don't think this information is necessary and we can get away with less communication.
+  //       Secondly, this code appears multiple times over the whole codebase, we should just refactor it into a function or class.
+  int rank{};
+  MPI_Comm_rank(ovlp_ids.first->communicator(), &rank);
+  const auto &ovlp_paridxs = *ovlp_ids.second;
+  IdentifyBoundaryDataHandle ibdh(Aovlp, ovlp_paridxs, rank);
+  communicator.forward(ibdh);
+  auto boundaryMask = ibdh.getBoundaryMask();
+
+  std::vector<int> boundary_dst(ovlp_paridxs.size(), std::numeric_limits<int>::max() - 1);
+  for (const auto &idxpair : ovlp_paridxs) {
+    auto li = idxpair.local();
+    if (boundaryMask[li]) {
+      boundary_dst[li] = 0;
+    }
+  }
+
+  // Here we take 2 * overlap plus some safety margin because we compute the distance from the overlapping subdomain boundary
+  const auto overlap = ptree.get("overlap", 1);
+  for (int round = 0; round <= 2 * overlap + 3; ++round) {
+    for (int i = 0; i < boundary_dst.size(); ++i) {
+      for (auto cIt = Aovlp[i].begin(); cIt != Aovlp[i].end(); ++cIt) {
+        boundary_dst[i] = std::min(boundary_dst[i], boundary_dst[cIt.index()] + 1); // Increase distance from boundary by one
       }
     }
-    else {
-      for (auto ci = A[i].begin(); ci != A[i].end(); ++ci) {
-        if (dirichlet_mask_ovlp[ci.index()] > 0) {
-          *ci = 0.0;
+  }
+
+  Mat A; // The left-hand side of the eigenproblem. Will be set below depending on the requested type
+  Mat B; // The right-hand side of the eigenproblem. Will be set below depending on the requested type
+  if (geneo_type == "full" or geneo_type == "overlap") {
+    // For the full GenEO coarse space, we first need to assemble the Neumann matrix on the overlapping subdomain.
+    // This can be done by copying the Dirichlet matrix and applying the Neumann correction that we created
+    // earlier.
+    A = Aovlp;
+
+    for (const auto &triple : remote_ncorr_triples) {
+      // The triples use global indices, so we first have to convert them to local indices
+      // on the overlapping subdomain. Also, we might have received some indices that are
+      // outside of our overlapping subdomain, so we first have to check that.
+      if (ovlp_paridxs.exists(triple.row) && ovlp_paridxs.exists(triple.col)) {
+        auto lrow = ovlp_paridxs[triple.row].local();
+        auto lcol = ovlp_paridxs[triple.col].local();
+
+        A[lrow][lcol] -= triple.val;
+      }
+      else {
+        spdlog::trace("Global index ({}, {}) does not exist in subdomain", triple.row, triple.col);
+      }
+    }
+
+    for (std::size_t i = 0; i < A.N(); ++i) {
+      if (dirichlet_mask_ovlp[i] > 0) {
+        for (auto ci = A[i].begin(); ci != A[i].end(); ++ci) {
+          *ci = (ci.index() == i) ? 1.0 : 0.0;
+        }
+      }
+      else {
+        for (auto ci = A[i].begin(); ci != A[i].end(); ++ci) {
+          if (dirichlet_mask_ovlp[ci.index()] > 0) {
+            *ci = 0.0;
+          }
         }
       }
     }
-  }
 
-  // Prepare the preconditioner for the eigenproblem
-  using Prec = Dune::SeqILDL<Mat, Vec, Vec>;
-  std::unique_ptr<Prec> prec{nullptr};
-  if (ptree.get("prec_eigensolver", false)) {
-    // for (std::size_t i = 0; i < A.N(); ++i) {
-    //   A[i][i] += 0.01;
-    // }
-    prec = std::make_unique<Prec>(A, 1.0);
-    // for (std::size_t i = 0; i < A.N(); ++i) {
-    //   A[i][i] -= 0.01;
-    // }
-  }
+    // Now we have the Neumann matrix on the overlapping subdomain. This is the left-hand side of the eigenproblem.
+    // The right-hand side is constructed from this matrix, its exact form depends on the requested GenEO type. In
+    // "full" case, we're almost done, we just need to multipliy B with the partiton of unity from both sides. In the
+    // "overlap" case (the classical GenEO coarse space), we need to eliminate all dofs that are not in the overlap.
+    B = A;
+    // This is the classical GenEO coarse space. Here, we have to eliminate all interior dofs of the B matrix.
+    if (geneo_type == "overlap") {
+      // Elminate all entries that correspond to dofs with a distance > 2*overlap from the boundary
+      for (auto ri = B.begin(); ri != B.end(); ++ri) {
+        // TODO: We can skip some rows early here (those that correspond to dofs in the overlap with no neighbours in the interior)
+        for (auto ci = ri->begin(); ci != ri->end(); ++ci) {
+          if (boundary_dst.at(ri.index()) > 2 * overlap or boundary_dst.at(ci.index()) > 2 * overlap) {
+            *ci = 0;
+          }
+        }
+      }
 
-  // Now we have the Neumann matrix on the overlapping subdomain. This is the left-hand side of the eigenproblem.
-  // The right-hand side is constructed from this matrix by multiplying it from both sides with the partition of unity.
-  auto B = A;
+      // Finally apply the corrections to get Neumann boundary conditions on the interior boundary
+      for (const auto &triple : own_ncorr_triples) {
+        if (boundary_dst.at(triple.row) == 2 * overlap and boundary_dst.at(triple.col) == 2 * overlap) {
+          B[triple.row][triple.col] -= triple.val;
+        }
+        else {
+          spdlog::get("all_ranks")->warn("Unexpected triple at distance ({}, {})", boundary_dst.at(triple.row), boundary_dst.at(triple.col));
+        }
+      }
 
-  // if (own_ncorr_triples.size() > 0) {
-  //   // Here, we zero the matrix first and then copy the values that we need back.
-  //   B = 0;
-  //   Vec interior_marker(ovlp_paridxs.size());
-  //   interior_marker = 1;
-  //   advdh.setVec(interior_marker);
-  //   communicator.forward(advdh);
+      // Dirichlet conditions
+      for (std::size_t i = 0; i < B.N(); ++i) {
+        if (dirichlet_mask_ovlp[i] > 0) {
+          for (auto ci = B[i].begin(); ci != B[i].end(); ++ci) {
+            *ci = (ci.index() == i) ? 1.0 : 0.0;
+          }
+        }
+        else {
+          for (auto ci = B[i].begin(); ci != B[i].end(); ++ci) {
+            if (dirichlet_mask_ovlp[ci.index()] > 0) {
+              *ci = 0.0;
+            }
+          }
+        }
+      }
+    }
 
-  //   for (auto ri = B.begin(); ri != B.end(); ++ri) {
-  //     for (auto ci = ri->begin(); ci != ri->end(); ++ci) {
-  //       if (interior_marker[ci.index()] > 1) {
-  //         *ci = A[ri.index()][ci.index()];
-  //       }
-  //     }
-  //   }
-
-  //   // And apply the Neumann corrections in the interior boundary
-  //   for (const auto &triple : own_ncorr_triples) {
-  //     auto row = ovlp_paridxs[triple.row].local();
-  //     auto col = ovlp_paridxs[triple.col].local();
-  //     B[row][col] -= triple.val; // TODO: Store the triples using local indices
-
-  //     if (interior_marker[row] > 1) {
-  //       std::cout << "Subtracting from overlap region\n";
-  //     }
-  //   }
-
-  //   for (std::size_t i = 0; i < B.N(); ++i) {
-  //     if (dirichlet_mask_ovlp[i] > 0) {
-  //       for (auto ci = B[i].begin(); ci != B[i].end(); ++ci) {
-  //         *ci = (ci.index() == i) ? 1.0 : 0.0;
-  //       }
-  //     }
-  //     else {
-  //       for (auto ci = B[i].begin(); ci != B[i].end(); ++ci) {
-  //         if (dirichlet_mask_ovlp[ci.index()] > 0) {
-  //           *ci = 0.0;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-  // finally multiply it with the partition of unity from the left and right
-  for (auto ri = B.begin(); ri != B.end(); ++ri) {
-    for (auto ci = ri->begin(); ci != ri->end(); ++ci) {
-      *ci *= pou[ri.index()] * pou[ci.index()];
+    // In both cases, the right-hand side matrix has to multiplied by the partition of unity from the left and right.
+    for (auto ri = B.begin(); ri != B.end(); ++ri) {
+      for (auto ci = ri->begin(); ci != ri->end(); ++ci) {
+        *ci *= pou[ri.index()] * pou[ci.index()];
+      }
     }
   }
+  else if (geneo_type == "ring") {
+    // In this case, we solve the eigenvalue problem only in ther overlapping region (which we will refer to here as the ring).
+    // The solution on the whole overlapping subdomain is then obtained by extending each eigenvector to the interior in a
+    // certain way (see below, after the solution of the eigenproblem).
+
+    // First we have to create the matrix on the ring. To this end, we first need to identify all degrees of freedom inside the ring.
+    std::vector<std::size_t> ring_to_subdomain(boundary_dst.size());
+    std::size_t cnt = 0;
+    for (std::size_t i = 0; i < boundary_dst.size(); ++i) {
+      if (boundary_dst[i] <= 2 * overlap) {
+        ring_to_subdomain[cnt++] = i;
+      }
+    }
+    ring_to_subdomain.resize(cnt);
+
+    // We create a ring-to-subdomain mapping by simply sorting this list of dofs
+    std::sort(ring_to_subdomain.begin(), ring_to_subdomain.end());
+
+    // We also create the inverse mapping (subdomain-to-ring)
+    std::unordered_map<std::size_t, std::size_t> subdomain_to_ring;
+    subdomain_to_ring.reserve(ring_to_subdomain.size());
+    for (std::size_t i = 0; i < ring_to_subdomain.size(); ++i) {
+      subdomain_to_ring[ring_to_subdomain[i]] = i;
+    }
+
+    // Now create the matrix
+    auto avg = Aovlp.nonzeroes() / Aovlp.N() + 2;
+    auto N = ring_to_subdomain.size();
+    A.setBuildMode(Mat::implicit);
+    A.setImplicitBuildModeParameters(avg, 0.2);
+    A.setSize(N, N);
+
+    for (auto ri = Aovlp.begin(); ri != Aovlp.end(); ++ri) {
+      for (auto ci = ri->begin(); ci != ri->end(); ++ci) {
+        if (subdomain_to_ring.contains(ri.index()) && subdomain_to_ring.contains(ci.index())) {
+          A.entry(subdomain_to_ring[ri.index()], subdomain_to_ring[ci.index()]) = 0.0;
+        }
+      }
+    }
+    A.compress();
+
+    for (auto ri = A.begin(); ri != A.end(); ++ri) {
+      for (auto ci = ri->begin(); ci != ri->end(); ++ci) {
+        *ci = Aovlp[ring_to_subdomain[ri.index()]][ring_to_subdomain[ci.index()]];
+      }
+    }
+
+    // We again need to apply the Neumann corrections. First do the "outside" boundary ...
+    for (const auto &triple : remote_ncorr_triples) {
+      // The triples use global indices, so we first have to convert them to local indices
+      // on the overlapping subdomain. Also, we might have received some indices that are
+      // outside of our overlapping subdomain, so we first have to check that.
+      if (ovlp_paridxs.exists(triple.row) && ovlp_paridxs.exists(triple.col)) {
+        auto lrow = ovlp_paridxs[triple.row].local();
+        auto lcol = ovlp_paridxs[triple.col].local();
+
+        // Check if the given entry corresponds to a dof inside the ring
+        if (subdomain_to_ring.contains(lrow) && subdomain_to_ring.contains(lcol)) {
+          A[subdomain_to_ring[lrow]][subdomain_to_ring[lcol]] -= triple.val;
+        }
+      }
+      else {
+        spdlog::trace("Global index ({}, {}) does not exist in subdomain", triple.row, triple.col);
+      }
+    }
+    // ... then the "inside" boundary
+    for (const auto &triple : own_ncorr_triples) {
+      if (boundary_dst.at(triple.row) == 2 * overlap and boundary_dst.at(triple.col) == 2 * overlap) {
+        if (subdomain_to_ring.contains(triple.row) && subdomain_to_ring.contains(triple.col)) {
+          A[subdomain_to_ring[triple.row]][subdomain_to_ring[triple.col]] -= triple.val;
+        }
+      }
+      else {
+        spdlog::get("all_ranks")->warn("Unexpected triple at distance ({}, {})", boundary_dst.at(triple.row), boundary_dst.at(triple.col));
+      }
+    }
+
+    // Dirichlet conditions
+    for (std::size_t i = 0; i < A.N(); ++i) {
+      if (dirichlet_mask_ovlp[ring_to_subdomain[i]] > 0) {
+        for (auto ci = A[i].begin(); ci != A[i].end(); ++ci) {
+          *ci = (ci.index() == i) ? 1.0 : 0.0;
+        }
+      }
+      else {
+        for (auto ci = A[i].begin(); ci != A[i].end(); ++ci) {
+          if (dirichlet_mask_ovlp[ring_to_subdomain[ci.index()]] > 0) {
+            *ci = 0.0;
+          }
+        }
+      }
+    }
+
+    B = A; // B starts as A, but has to be multiplied by the partition of unity
+
+    for (auto ri = B.begin(); ri != B.end(); ++ri) {
+      for (auto ci = ri->begin(); ci != ri->end(); ++ci) {
+        *ci *= pou[ring_to_subdomain[ri.index()]] * pou[ring_to_subdomain[ci.index()]];
+      }
+    }
+  }
+  else {
+    spdlog::error("Unknown GenEO type '{}', aborting");
+    MPI_Abort(MPI_COMM_WORLD, 3);
+  }
+
   Logger::get().endEvent(neumann_corr);
 
   //  Regularize
-  for (std::size_t i = 0; i < B.N(); ++i) {
-    B[i][i] += 0.0001;
-  }
+  // for (std::size_t i = 0; i < B.N(); ++i) {
+  //   B[i][i] += 0.0001;
+  // }
 
-  int rank;
-  MPI_Comm_rank(ovlp_ids.first->communicator(), &rank);
   if (rank == 0) {
     Dune::writeMatrixToMatlab(A, "A.mat");
     Dune::writeMatrixToMatlab(B, "B.mat");
@@ -174,7 +294,7 @@ std::vector<Vec> buildGenEOCoarseSpace(const RemoteIndices &ovlp_ids, const Mat 
 
   Logger::get().startEvent(eigensolver_event);
   spdlog::info("Solving generalized eigenvalue problem for GenEO");
-  std::vector<Vec> eigenvectors = solveGEVP<Vec>(A, B, eigensolver, ptree.get("nev", 10), prec.get());
+  std::vector<Vec> eigenvectors = solveGEVP<Vec>(A, B, eigensolver, ptree.get("nev", 10));
 
   for (auto &vec : eigenvectors) {
     for (std::size_t i = 0; i < vec.N(); ++i) {
@@ -186,6 +306,10 @@ std::vector<Vec> buildGenEOCoarseSpace(const RemoteIndices &ovlp_ids, const Mat 
       }
     }
   }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Abort(MPI_COMM_WORLD, 0);
+
   Logger::get().endEvent(eigensolver_event);
   return eigenvectors;
 }
