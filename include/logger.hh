9caeb1debb01d312be02f99bb358193a3ffdb003
Nils Friess <nils.friess@gmail.com>
2025-03-24 14:20:58 +0100

    Add implementation of setup_loggers

--- include/logger.hh
+++ include/logger.hh
@@ -7,6 +7,9 @@
 #include <string>
 #include <vector>
 
+#include "spdlog/cfg/argv.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 // TODO: This is a bit sketchy because we preallocate memory for the families and events;
 //       as soon as either of the vectors has to be resized, all pointers to the events
 //       become invalid. For now it works well enough.
@@ -236,4 +239,22 @@ private:
 
   std::vector<Family> families;
 };
-void setup_loggers(int, int &, char **&);
+
+inline void setup_loggers(int rank, int &argc, char **&argv)
+{
+  // Default logger (only active on rank 0)
+  if (rank == 0) {
+    spdlog::set_pattern("[%H:%M:%S.%e] [%^%l%$:0] %v"); // Default format
+    spdlog::cfg::load_argv_levels(argc, argv);
+  }
+  else {
+    spdlog::set_level(spdlog::level::off); // Silence logs on other ranks
+  }
+
+  // Logger for all ranks
+  auto all_ranks_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  auto all_ranks_logger = std::make_shared<spdlog::logger>("all_ranks", all_ranks_sink);
+  all_ranks_logger->set_level(spdlog::level::info);
+  all_ranks_logger->set_pattern("[%H:%M:%S.%e] [%^%l:%$" + std::to_string(rank) + "] %v");
+  spdlog::register_logger(all_ranks_logger);
+}
