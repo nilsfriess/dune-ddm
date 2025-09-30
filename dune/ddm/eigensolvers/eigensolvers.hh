a60eb872849a3459d6c15ab24b18f4754260a1c0
Nils Friess <nils.friess@gmail.com>
2025-09-30 14:34:53 +0200

    chore(vendor): add Spectra 1.2.0 headers
    Vendor Spectra headers under extern/spectra-1.2.0 for the Spectra-based eigensolver backend.

--- dune/ddm/eigensolvers/eigensolvers.hh
+++ dune/ddm/eigensolvers/eigensolvers.hh
@@ -1,68 +1,41 @@
 #pragma once
 
+#include "dune/common/exceptions.hh"
+#include "eigensolver_params.hh"
+#include "spectra.hh"
+#include "subspace_iteration.hh"
+
 #include <dune/common/parametertree.hh>
 #include <dune/istl/bcrsmatrix.hh>
 #include <dune/istl/bvector.hh>
-
 #include <dune/istl/solver.hh>
 #include <vector>
 
-enum class Eigensolver { Spectra, BLOPEX };
-
-// Helper to convert floats to string because std::to_string outputs with low precision
-template <typename T>
-std::string to_string_with_precision(const T a_value)
+template <class Mat>
+std::vector<Dune::BlockVector<Dune::FieldVector<double, 1>>> solve_gevp(const Mat& A, Mat& B, const Dune::ParameterTree& ptree)
 {
-  std::ostringstream out;
-  out << a_value;
-  return std::move(out).str();
+  EigensolverParams params(ptree);
+
+  if (params.type == EigensolverParams::Type::SubspaceIteration) { //
+    return subspace_iteration(A, B, params);
+  }
+  else if (params.type == EigensolverParams::Type::Spectra) {
+    return spectra_gevp(A, B, params);
+  }
+  // else if (params.type == EigensolverParams::Type::RAES) {
+  //   std::vector<Dune::BlockVector<Dune::FieldVector<double, 1>>> evecs(params.nev);
+  //   std::vector<double> evals(params.nev);
+  //   GeneralizedInverse(A, B, params.shift, 0.0, params.tolerance, params.maxit, params.nev, evals, evecs, 1);
+  //   std::cout << "Computed eigenvalues using Rayleigh quotients:\n";
+  //   for (std::size_t i = 0; i < params.nev; ++i) std::cout << evals[i] << "  ";
+  //   std::cout << "\n";
+  //   return evecs;
+  // }
+  else {
+    DUNE_THROW(Dune::NotImplemented, "Eigensolver not implemented");
+  }
 }
 
-// template <class Prec>
-// void applyPreconditionerFake(void *T_, void *r_, void *Tr_)
-// {
-//   auto *T = static_cast<Prec *>(T_);
-//   auto *r = static_cast<mv_TempMultiVector *>(r_);
-//   auto *Tr = static_cast<mv_TempMultiVector *>(Tr_);
-
-//   for (std::size_t i = 0; i < r->numVectors; ++i) {
-//     if (r->mask && r->mask[i] == 0) {
-//       continue;
-//     }
-
-//     auto *v = static_cast<Vector *>(r->vector[i]);
-//     auto *res = static_cast<Vector *>(Tr->vector[i]);
-
-//     Dune::InverseOperatorResult ior;
-//     T->apply(*res, *v, ior);
-//   }
-// }
-
-// template <class Prec>
-// void applyPreconditioner(void *T_, void *r_, void *Tr_)
-// {
-//   auto *T = static_cast<Prec *>(T_);
-//   auto *r = static_cast<Multivec *>(r_);
-//   auto *Tr = static_cast<Multivec *>(Tr_);
-
-//   Dune::BlockVector<Dune::FieldVector<double, 1>> v(r->N);
-//   Dune::BlockVector<Dune::FieldVector<double, 1>> d(r->N);
-
-//   Dune::InverseOperatorResult ior;
-//   for (std::size_t i = 0; i < r->active_indices.size(); ++i) {
-//     auto *rstart = r->entries.data() + r->active_indices[i] * r->N;
-//     auto *Trstart = Tr->entries.data() + Tr->active_indices[i] * Tr->N;
-
-//     std::copy(rstart, rstart + r->N, d.begin());
-//     v = 0;
-
-//     Dune::InverseOperatorResult res;
-//     T->apply(v, d, res);
-
-//     std::copy(v.begin(), v.end(), Trstart);
-//   }
-// }
-
-std::vector<Dune::BlockVector<Dune::FieldVector<double, 1>>> solveGEVP(const Dune::BCRSMatrix<Dune::FieldMatrix<double, 1>> &A, const Dune::BCRSMatrix<Dune::FieldMatrix<double, 1>> &B,
-                                                                       Eigensolver eigensolver, const Dune::ParameterTree &ptree,
-                                                                       Dune::Preconditioner<Dune::BlockVector<Dune::FieldVector<double, 1>>, Dune::BlockVector<Dune::FieldVector<double, 1>>> *prec = nullptr);
+// std::vector<Dune::BlockVector<Dune::FieldVector<double, 1>>>
+// solveGEVP(const Dune::BCRSMatrix<Dune::FieldMatrix<double, 1>> &A, const Dune::BCRSMatrix<Dune::FieldMatrix<double, 1>> &B, Eigensolver eigensolver, const Dune::ParameterTree &ptree,
+//           Dune::Preconditioner<Dune::BlockVector<Dune::FieldVector<double, 1>>, Dune::BlockVector<Dune::FieldVector<double, 1>>> *prec = nullptr);
