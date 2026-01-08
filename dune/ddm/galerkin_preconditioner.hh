9c9e1aecf1f07ae77f1748248ea938d1fc20b24e
Nils Friess <nils.friess@gmail.com>
2026-01-08 20:09:05 +0100

    Refactor GalerkinPreconditioner to use a Communication class for improved parallel handling; remove unused gather-scatter helpers and update logging messages

--- dune/ddm/galerkin_preconditioner.hh
+++ dune/ddm/galerkin_preconditioner.hh
@@ -44,25 +44,9 @@
     The coarse problem size equals the total number of template vectors across all ranks.
     For good scalability, this should grow slowly with the number of MPI processes.
  */
-template <class Vec>
+template <class Vec, class Communication>
 class GalerkinPreconditioner : public Dune::Preconditioner<Vec, Vec> {
   using Solver = Dune::InverseOperator<Vec, Vec>;
-
-  /** @brief Gather-scatter helper for adding values during communication */
-  struct AddGatherScatter {
-    using DataType = double;
-
-    static DataType gather(const Vec& x, std::size_t i) { return x[i]; }
-    static void scatter(Vec& x, DataType v, std::size_t i) { x[i] += v; }
-  };
-
-  /** @brief Gather-scatter helper for copying values during communication */
-  struct CopyGatherScatter {
-    using DataType = double;
-
-    static DataType gather(const Vec& x, std::size_t i) { return x[i]; }
-    static void scatter(Vec& x, DataType v, std::size_t i) { x[i] = v; }
-  };
 
   /** @brief Helper class for distributing vectors across MPI ranks during communication */
   struct VecDistributor {
@@ -111,7 +95,7 @@ class GalerkinPreconditioner : public Dune::Preconditioner<Vec, Vec> {
       const auto& [rank, v] = data;
 
       if (vd.others.count(rank) == 0) {
-        logger::error_all("Rank {} is no neighbour", rank);
+        logger::error_all("CopyGatherScatterWithRank: Rank {} is no neighbour", rank);
         MPI_Abort(MPI_COMM_WORLD, 17);
       }
       vd.others[rank][i] = v;
@@ -131,22 +115,23 @@ public:
    *
    * @throws Dune::Exception if no template vectors are provided or if template vector size doesn't match matrix size
    */
-  template <class Mat, class RemoteIndices>
-  GalerkinPreconditioner(const Mat& A, const std::vector<Vec>& ts, const RemoteIndices& remoteids, const Dune::ParameterTree& ptree, const std::string& subtree_name = "galerkin")
-      : n(A.N())
+  template <class Mat>
+  GalerkinPreconditioner(const Mat& A, const std::vector<Vec>& ts, std::shared_ptr<Communication> comm, const Dune::ParameterTree& ptree, const std::string& subtree_name = "galerkin")
+      : comm(std::move(comm))
+      , n(A.N())
       , d_ovlp(n)
       , x_ovlp(n)
       , num_t(ts.size())
   {
     register_log_events();
-    build_communication_interfaces(remoteids);
+    // build_communication_interfaces(remoteids);
 
     if (ts.size() == 0) DUNE_THROW(Dune::Exception, "Must at least pass one template vector");
 
     if (ts[0].N() != A.N()) DUNE_THROW(Dune::Exception, "Template vectors must match size of matrix");
 
     std::size_t max_num_t = ts.size();
-    MPI_Allreduce(MPI_IN_PLACE, &max_num_t, 1, MPI_UNSIGNED_LONG, MPI_MAX, comm);
+    MPI_Allreduce(MPI_IN_PLACE, &max_num_t, 1, MPI_UNSIGNED_LONG, MPI_MAX, this->comm->communicator());
 
     Vec zero(ts[0].N());
     zero = 0;
@@ -155,7 +140,7 @@ public:
 
     logger::debug("Setting up GalerkinPreconditioner with {} template vector{} (max. one rank has is {})", num_t, (num_t == 1 ? "" : "s"), max_num_t);
 
-    build_solver(A, remoteids, ptree, subtree_name);
+    build_solver(A, ptree, subtree_name);
   }
 
   Dune::SolverCategory::Category category() const override { return Dune::SolverCategory::nonoverlapping; }
@@ -167,11 +152,14 @@ public:
   {
     Logger::ScopedLog se(apply_event);
 
+    auto mpicomm = comm->communicator();
+    int rank = comm->communicator().rank();
+
     // 1. Copy local values from non-overlapping to overlapping defect
     for (std::size_t i = 0; i < d.N(); ++i) d_ovlp[i] = d[i];
 
     // 1.5 Get remaining values from other ranks
-    owner_copy_comm.forward<CopyGatherScatter>(d_ovlp);
+    comm->copyOwnerToAll(d_ovlp, d_ovlp);
 
     // 2. Compute local contribution of coarse defect
     std::vector<double> d_local(num_t, 0);
@@ -180,7 +168,7 @@ public:
 
     // 3. Gather defect values on rank 0
     Vec d0(total_num_t);
-    MPI_Gatherv(d_local.data(), static_cast<int>(d_local.size()), MPI_DOUBLE, d0.data(), num_t_per_rank.data(), offset_per_rank.data(), MPI_DOUBLE, 0, comm);
+    MPI_Gatherv(d_local.data(), static_cast<int>(d_local.size()), MPI_DOUBLE, d0.data(), num_t_per_rank.data(), offset_per_rank.data(), MPI_DOUBLE, 0, mpicomm);
 
     // 4. Solve on rank 0
     Vec x0(total_num_t);
@@ -192,15 +180,14 @@ public:
 
     // 5. Scatter the local solution back
     std::vector<double> coarse_solution(num_t);
-    MPI_Scatterv(x0.data(), num_t_per_rank.data(), offset_per_rank.data(), MPI_DOUBLE, coarse_solution.data(), num_t, MPI_DOUBLE, 0, comm);
+    MPI_Scatterv(x0.data(), num_t_per_rank.data(), offset_per_rank.data(), MPI_DOUBLE, coarse_solution.data(), num_t, MPI_DOUBLE, 0, mpicomm);
 
     // 6. Prolongate
     x_ovlp = 0;
     for (int k = 0; k < num_t; ++k)
       for (std::size_t j = 0; j < x_ovlp.N(); ++j) x_ovlp[j] += coarse_solution[k] * restr_vecs[k][j];
 
-    // advdh.setVec(x_ovlp);
-    all_all_comm.forward<AddGatherScatter>(x_ovlp);
+    comm->addOwnerCopyToAll(x_ovlp, x_ovlp);
 
     // 7. Restrict the solution to the non-overlapping subdomain
     for (std::size_t i = 0; i < x.size(); ++i) x[i] = x_ovlp[i];
@@ -218,28 +205,6 @@ private:
   }
 
   /**
-   * @brief Build communication interfaces for parallel operations
-   * @param ris Remote parallel indices describing the overlapping index set and communication patterns
-   */
-  template <class RemoteIndices>
-  void build_communication_interfaces(const RemoteIndices& remoteids)
-  {
-    MPI_Comm_dup(remoteids.communicator(), &comm);
-    MPI_Comm_rank(comm, &rank);
-    MPI_Comm_size(comm, &size);
-
-    const AttributeSet allAttributes{Attribute::owner, Attribute::copy};
-    const AttributeSet ownerAttribute{Attribute::owner};
-    const AttributeSet copyAttribute{Attribute::copy};
-
-    all_all_interface.build(remoteids, allAttributes, allAttributes);
-    all_all_comm.build<Vec>(all_all_interface);
-
-    owner_copy_interface.build(remoteids, ownerAttribute, copyAttribute);
-    owner_copy_comm.build<Vec>(owner_copy_interface);
-  }
-
-  /**
    * @brief Build the coarse space solver by assembling R A R^T and factorizing it
    * @param A The overlapping matrix used to compute the Galerkin product
    *
@@ -251,10 +216,13 @@ private:
    */
   // TODO: Remove some of the logging, this was just added to find out where the most time is spent,
   //       because this function can become the bottleneck for large simulations.
-  template <class Mat, class RemoteIndices>
-  void build_solver(const Mat& A, const RemoteIndices& remoteids, const Dune::ParameterTree& ptree, const std::string &subtree_name)
+  template <class Mat>
+  void build_solver(const Mat& A, const Dune::ParameterTree& ptree, const std::string& subtree_name)
   {
     Logger::ScopedLog se(build_solver_event);
+    auto mpicomm = comm->communicator();
+    int rank = comm->communicator().rank();
+    int size = comm->communicator().size();
 
     auto* comm_begin_event = Logger::get().registerOrGetEvent("GalerkinPrec", "begin comm");
     // auto *comm_end_event = Logger::get().registerOrGetEvent("GalerkinPrec", "end comm");
@@ -265,17 +233,19 @@ private:
     auto* prepare_event = Logger::get().registerOrGetEvent("GalerkinPrec", "prepare");
 
     Logger::get().startEvent(prepare_event);
-    Dune::GlobalLookupIndexSet glis(remoteids.sourceIndexSet());
+    comm->buildGlobalLookup();
+    const auto& glis = comm->globalLookup();
+    // Dune::GlobalLookupIndexSet glis(remoteids.sourceIndexSet());
 
     std::vector<bool> neighbours(size, false);
-    for (auto nid : remoteids.getNeighbours()) neighbours[nid] = true;
+    for (auto nid : comm->remoteIndices().getNeighbours()) neighbours[nid] = true;
 
     std::vector<bool> owner_mask(restr_vecs[0].N(), false);
-    for (std::size_t l = 0; l < owner_mask.size(); ++l) owner_mask[l] = glis.pair(l)->local().attribute() == Attribute::owner;
+    for (std::size_t l = 0; l < owner_mask.size(); ++l) owner_mask[l] = glis.pair(l)->local().attribute() == Dune::OwnerOverlapCopyAttributeSet::owner;
 
     // Find out how many template vectors each rank has and how large coarse matrix will be
     num_t_per_rank.resize(size);
-    MPI_Allgather(&num_t, 1, MPI_INT, num_t_per_rank.data(), 1, MPI_INT, comm);
+    MPI_Allgather(&num_t, 1, MPI_INT, num_t_per_rank.data(), 1, MPI_INT, mpicomm);
     total_num_t = std::accumulate(num_t_per_rank.begin(), num_t_per_rank.end(), 0UL);
 
     std::vector<double> my_rows_flat(static_cast<std::size_t>(num_t) * total_num_t);
@@ -286,7 +256,7 @@ private:
     std::exclusive_scan(num_t_per_rank.begin(), num_t_per_rank.end(), offset_per_rank.begin(), 0);
 
     Logger::get().endEvent(prepare_event);
-#if 1
+
     // TODO: Here we previously used a custom implementation of BufferedCommunicator that supports forwardBegin/forwardEnd to overlap computation and communication. This is currently disabled again to
     // make it work with a standard DUNE installation.
 
@@ -294,9 +264,13 @@ private:
 
     Vec zerovec(s);
     zerovec = 0;
-    std::vector<int> neighbour_vec(remoteids.getNeighbours().begin(), remoteids.getNeighbours().end());
+    std::vector<int> neighbour_vec(comm->remoteIndices().getNeighbours().begin(), comm->remoteIndices().getNeighbours().end());
     VecDistributor vd(zerovec, neighbour_vec, rank);
 
+    using AllSet = Communication::AllSet;
+    AllSet all_att;
+    Dune::Interface all_all_interface;
+    all_all_interface.build(comm->remoteIndices(), all_att, all_att);
     Dune::BufferedCommunicator bcomm;
     bcomm.build<VecDistributor>(all_all_interface);
 
@@ -352,56 +326,9 @@ private:
       for (int k = 0; k < num_t; ++k) my_rows_flat[(offset_per_rank[nb] + max_num_t - 1) * num_t + k] = restr_vecs[k] * y;
     }
     Logger::get().endEvent(local_remote_sp_event);
-#else
-    auto* dot_vecs_event = Logger::get().registerOrGetEvent("GalerkinPrec", "dot products");
-    auto* s_event = Logger::get().registerOrGetEvent("GalerkinPrec", "comm s");
-    auto* y_event = Logger::get().registerOrGetEvent("GalerkinPrec", "comm y");
-    auto* Asy_event = Logger::get().registerOrGetEvent("GalerkinPrec", "compute y=As");
-
-    for (int col = 0; col < size; ++col) {
-      for (std::size_t i = 0; i < num_t_per_rank[col]; ++i) {
-        if (col == rank) s = restr_vecs[i];
-        else s = 0;
-
-        Logger::get().startEvent(s_event);
-        all_all_comm.forward<AddGatherScatter>(s);
-        Logger::get().endEvent(s_event);
-
-        y = 0;
-        // Below, we're computing products of the form s' * A * r, for all combinations of restriction vectors s and r.
-        // This product will only be nonzero, if either we set some values in the 's' vector, or one of our neighbours
-        // set some values in their 's' vector. For a rank that we share no degrees of freedom with, this will always be
-        // zero and we can skip the computations. Unfortunately, we can't skip the communication.
-        // TODO: Rewrite the whole communication interface so we can actually skip the communication here.
-        if (col == rank or neighbours[col]) {
-          Logger::get().startEvent(Asy_event);
-          A.mv(s, y);
-
-          for (std::size_t l = 0; l < y.N(); ++l) y[l] *= owner_mask[l];
-          Logger::get().endEvent(Asy_event);
-        }
-
-        Logger::get().startEvent(y_event);
-        all_all_comm.forward<AddGatherScatter>(y);
-        Logger::get().endEvent(y_event);
-
-        // Again, we skip the computation if we know it will be zero. In fact, these dot products are often
-        // the longest taking step in this loop, so skipping unecessary computations here is crucial.
-        // TODO: Is this actually true? We have to wait for the other ranks anyways so I'm not sure if
-        //       saving some computations locally is really that important.
-
-        // Note that my_rows is initialised with zeros, so just skipping here is fine.
-        if (col == rank or neighbours[col]) {
-          Logger::get().startEvent(dot_vecs_event);
-          for (std::size_t k = 0; k < num_t; ++k) my_rows[k][offset_per_rank[col] + i] = restr_vecs[k] * y;
-          Logger::get().endEvent(dot_vecs_event);
-        }
-      }
-    }
-#endif
 
     Logger::get().startEvent(gather_A0);
-    auto a0 = std::make_shared<Mat>(gatherMatrixFromRowsFlat(my_rows_flat, total_num_t, comm));
+    auto a0 = std::make_shared<Mat>(gatherMatrixFromRowsFlat(my_rows_flat, total_num_t, mpicomm));
     Logger::get().endEvent(gather_A0);
 
     Logger::get().startEvent(factor_A0);
@@ -421,22 +348,16 @@ private:
     Logger::get().endEvent(factor_A0);
   }
 
-  std::shared_ptr<Solver> solver;             ///  Direct solver for the coarse problem (UMFPack by default)
-  std::vector<Vec> restr_vecs;                ///  Template vectors used to build the restriction matrix
-  std::size_t n;                              ///  Size of the overlapping index set
-  MPI_Comm comm{};                            ///  The MPI communicator
-  int rank{};                                 ///  The MPI rank
-  int size{};                                 ///  The MPI size
-  Vec d_ovlp;                                 ///  Overlapping defect vector for temporary storage
-  Vec x_ovlp;                                 ///  Overlapping solution vector for temporary storage
-  int num_t;                                  ///  Number of template vectors owned by this rank
-  int total_num_t{};                          ///  Total number of template vectors across all ranks
-  std::vector<int> num_t_per_rank;            ///  Number of template vectors per rank
-  std::vector<int> offset_per_rank;           ///  Offset for each rank's template vectors in global numbering
-  Dune::Interface all_all_interface;          ///  Communication interface for all-to-all communication
-  Dune::BufferedCommunicator all_all_comm;    ///  Buffered communicator for all-to-all communication
-  Dune::Interface owner_copy_interface;       ///  Communication interface for owner-to-copy communication
-  Dune::BufferedCommunicator owner_copy_comm; ///  Buffered communicator for owner-to-copy communication
-  Logger::Event* apply_event{};               ///  Logging event for timing the apply method
-  Logger::Event* build_solver_event{};        ///  Logging event for timing the solver building process
+  std::shared_ptr<Communication> comm;
+  std::shared_ptr<Solver> solver;   ///  Direct solver for the coarse problem (UMFPack by default)
+  std::vector<Vec> restr_vecs;      ///  Template vectors used to build the restriction matrix
+  std::size_t n;                    ///  Size of the overlapping index set
+  Vec d_ovlp;                       ///  Overlapping defect vector for temporary storage
+  Vec x_ovlp;                       ///  Overlapping solution vector for temporary storage
+  int num_t;                        ///  Number of template vectors owned by this rank
+  int total_num_t{};                ///  Total number of template vectors across all ranks
+  std::vector<int> num_t_per_rank;  ///  Number of template vectors per rank
+  std::vector<int> offset_per_rank; ///  Offset for each rank's template vectors in global numbering
+  Logger::Event* apply_event{};        ///  Logging event for timing the apply method
+  Logger::Event* build_solver_event{}; ///  Logging event for timing the solver building process
 };
