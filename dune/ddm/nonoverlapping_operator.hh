5066631b860b780bc5b88a649f9d16836f14a56d
Nils Friess <nils.friess@gmail.com>
2026-01-08 20:10:36 +0100

    Refactor NonOverlappingOperator and NonOverlappingScalarProduct to use a generic Communication class; remove NonOverlappingCommunicator and streamline communication methods

--- dune/ddm/nonoverlapping_operator.hh
+++ dune/ddm/nonoverlapping_operator.hh
@@ -8,149 +8,79 @@
 #include <dune/istl/operators.hh>
 #include <memory>
 
-class NonOverlappingCommunicator {
-  template <class X>
-  struct AddGatherScatter {
-    using DataType = typename Dune::CommPolicy<X>::IndexedType;
-
-    static DataType gather(const X &x, std::size_t i) { return x[i]; }
-    static void scatter(X &x, DataType v, std::size_t i) { x[i] += v; }
-  };
-
-public:
-  template <class RemoteIndices>
-  explicit NonOverlappingCommunicator(const RemoteIndices &ri) : cc(ri.communicator())
-  {
-    mask.resize(ri.sourceIndexSet().size(), 0);
-    for (const auto &idx : ri.sourceIndexSet()) {
-      mask[idx.local()] = (unsigned)(idx.local().attribute() == Attribute::owner);
-    }
-  }
-
-  const Dune::Communication<MPI_Comm> &communicator() const { return cc; }
-
-  template <class X>
-  void copyOwnerToAll(const X &, X &) const
-  {
-    DUNE_THROW(Dune::NotImplemented, "copyOwnerToAll");
-  }
-
-  template <class X>
-  void addOwnerCopyToOwnerCopy(const X &, X &) const
-  {
-    DUNE_THROW(Dune::NotImplemented, "addOwnerCopyToOwnerCopy");
-  }
-
-  template <class X>
-  void dot(const X &x, const X &y, double &res) const
-  {
-    res = 0;
-    for (typename X::size_type i = 0; i < x.size(); i++) {
-      res += x[i] * y[i] * mask[i];
-    }
-    res = cc.sum(res);
-  }
-
-  template <class X>
-  double norm(const X &x) const
-  {
-    double res = 0;
-    dot(x, x, res);
-    return std::sqrt(res);
-  }
-
-private:
-  std::vector<unsigned> mask;
-  Dune::Communication<MPI_Comm> cc;
-};
-
-template <class Mat, class X, class Y = X>
+template <class Mat, class X, class Y, class Communication>
 class NonOverlappingOperator : public Dune::AssembledLinearOperator<Mat, X, Y> {
-private:
-  struct AddGatherScatter {
-    using DataType = typename Dune::CommPolicy<X>::IndexedType;
-
-    static DataType gather(const X &x, std::size_t i) { return x[i]; }
-    static void scatter(X &x, DataType v, std::size_t i) { x[i] += v; }
-  };
-
 public:
   using domain_type = X;
   using range_type = Y;
   using matrix_type = Mat;
-  using communication_type = NonOverlappingCommunicator;
+  using communication_type = Communication;
   using field_type = typename X::field_type;
 
-  template <class RemoteIndices>
-  NonOverlappingOperator(std::shared_ptr<Mat> A, const RemoteIndices &ri) : A(std::move(A)), comm(std::make_shared<communication_type>(ri))
+  NonOverlappingOperator(std::shared_ptr<Mat> A, std::shared_ptr<Communication> comm)
+      : A(std::move(A))
+      , comm(std::move(comm))
   {
-    const AttributeSet allAttributes{Attribute::owner, Attribute::copy};
-    all_all_interface.build(ri, allAttributes, allAttributes);
-    communicator = std::make_shared<Dune::BufferedCommunicator>();
-    communicator->build<X>(all_all_interface);
   }
 
-  NonOverlappingOperator(const NonOverlappingOperator &) = delete;
-  NonOverlappingOperator(const NonOverlappingOperator &&) = delete;
-  NonOverlappingOperator &operator=(const NonOverlappingOperator &) = delete;
-  NonOverlappingOperator &operator=(const NonOverlappingOperator &&) = delete;
+  NonOverlappingOperator(const NonOverlappingOperator&) = delete;
+  NonOverlappingOperator(const NonOverlappingOperator&&) = delete;
+  NonOverlappingOperator& operator=(const NonOverlappingOperator&) = delete;
+  NonOverlappingOperator& operator=(const NonOverlappingOperator&&) = delete;
   ~NonOverlappingOperator() = default;
 
   Dune::SolverCategory::Category category() const override { return Dune::SolverCategory::nonoverlapping; }
 
-  void apply(const X &x, Y &y) const override
+  void apply(const X& x, Y& y) const override
   {
     // Logger::ScopedLog sl(apply_event);
     A->mv(x, y);
-    communicator->forward<AddGatherScatter>(y);
+    comm->addOwnerCopyToOwnerCopy(y, y);
   }
 
-  void applyscaleadd(field_type alpha, const X &x, Y &y) const override
+  void applyscaleadd(field_type alpha, const X& x, Y& y) const override
   {
     // Logger::ScopedLog sl(applyscaleadd_event);
     // Since y is already consistent, we only communicate the result of alpha*A*x.
     auto y1 = y;
     y = 0;
     A->usmv(alpha, x, y);
-    communicator->forward<AddGatherScatter>(y);
+    comm->addOwnerCopyToOwnerCopy(y, y);
     y += y1;
   }
 
-  const Mat &getmat() const override { return *A; }
+  const Mat& getmat() const override { return *A; }
 
-  const communication_type &getCommunication() const { return *comm; }
+  const communication_type& getCommunication() const { return *comm; }
   std::shared_ptr<communication_type> getCommunicationPtr() const { return comm; }
 
 private:
   std::shared_ptr<Mat> A;
-  Dune::Interface all_all_interface;
-  std::shared_ptr<Dune::BufferedCommunicator> communicator;
-
   std::shared_ptr<communication_type> comm;
-
-  // Logger::Event *apply_event;
-  // Logger::Event *applyscaleadd_event;
 };
 
 namespace Dune {
-template <class X>
+template <class X, class C>
 class NonOverlappingScalarProduct : public Dune::ScalarProduct<X> {
 public:
   using base = Dune::ScalarProduct<X>;
-  using communication_type = NonOverlappingCommunicator;
+  using communication_type = C;
   using field_type = typename base::field_type;
   using real_type = typename base::real_type;
 
-  explicit NonOverlappingScalarProduct(std::shared_ptr<communication_type> comm) : comm(std::move(comm)) {}
+  explicit NonOverlappingScalarProduct(std::shared_ptr<communication_type> comm)
+      : comm(std::move(comm))
+  {
+  }
 
-  field_type dot(const X &x, const X &y) const override
+  field_type dot(const X& x, const X& y) const override
   {
     field_type res{};
     comm->dot(x, y, res);
     return res;
   }
 
-  real_type norm(const X &x) const override { return comm->norm(x); }
+  real_type norm(const X& x) const override { return comm->norm(x); }
 
   SolverCategory::Category category() const override { return SolverCategory::nonoverlapping; }
 
@@ -158,9 +88,9 @@ private:
   std::shared_ptr<communication_type> comm;
 };
 
-template <class M, class X, class Y>
-std::shared_ptr<NonOverlappingScalarProduct<X>> createScalarProduct(const std::shared_ptr<NonOverlappingOperator<M, X, Y>> &op)
+template <class M, class X, class Y, class C>
+std::shared_ptr<NonOverlappingScalarProduct<X, C>> createScalarProduct(const std::shared_ptr<NonOverlappingOperator<M, X, Y, C>>& op)
 {
-  return std::make_shared<NonOverlappingScalarProduct<X>>(op->getCommunicationPtr());
+  return std::make_shared<NonOverlappingScalarProduct<X, C>>(op->getCommunicationPtr());
 }
 } // namespace Dune
